c1fed36cd75ab3b385747c86757dcc9373862925
Ratson <github@ratson.name>
2022-07-29 14:27:17 +0800

    feat: exit with non-zero code for unhandled error

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+import { exit } from "node:process";
 import cli from "./index.js";
 
-cli().catch(console.error);
+cli().catch((err) => {
+  console.error(err);
+  const { exitCode = 1 } = err;
+  exit(exitCode);
+});
